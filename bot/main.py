dd99513f8897c3bc8625b410ba7c68c13bd9d80c
VSS <sandybreak3405@gmail.com>
2024-10-06 13:56:41 +0300

    ADD main.py

--- bot/main.py
+++ bot/main.py
@@ -9,20 +9,22 @@ from aiogram import Bot, Dispatcher
 from dotenv import load_dotenv
 
 from models.long_messages import LONG_DESCRIPTION, SHORT_DESCRIPTION
-from routers import main_router, trade_offer_wish
+
+from admin import admin_panel
+from routers import commands, registration, actions, trade_offer_wish
 
 from config import TELEGRAM_TOKEN
 
 async def set_commands_and_description(bot: Bot) -> None:
     commands = [
-    BotCommand(
-        command="/cancel",
-        description="Отменить действие"
-		),
-    BotCommand(
-        command="/menu",
-        description="Меню"
-		)
+        BotCommand(
+            command="/menu",
+            description="Меню"
+	    ),
+        BotCommand(
+            command="/cancel",
+            description="Отменить действие"
+	    )
     ]
 
     await bot.set_my_description(description=LONG_DESCRIPTION)
@@ -31,7 +33,7 @@ async def set_commands_and_description(bot: Bot) -> None:
     
     
 async def main():
-    load_dotenv()#Потом убрать надо
+    load_dotenv()#Для локальных запусков
     
     logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%d-%m %H:%M')
     
@@ -40,7 +42,11 @@ async def main():
     
     await set_commands_and_description(bot)
     
-    dp.include_router(main_router.router)
+    dp.include_router(commands.router)
+    dp.include_router(registration.router)
+    dp.include_router(actions.router)
+    dp.include_router(admin_panel.router)
+
     dp.include_router(trade_offer_wish.router)
 
     logging.warning("BOT STARTED")
